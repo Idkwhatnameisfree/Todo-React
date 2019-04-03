bdc49d693e8c7fa61cee9db8829acf271b6abad7
Idkwhatnameisfree <giuliano.wendt@gmail.com>
2019-04-03 14:03:52 +0200

    Updates

---
+++ precache-manifest.fd428610cd1f651ac829a861549748e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Todo-React/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e0248b7852b03c8abce4",
+    "url": "/Todo-React/static/js/runtime~main.65cb480b.js"
+  },
+  {
+    "revision": "fd70799a4309e7db155a",
+    "url": "/Todo-React/static/js/main.f82b8318.chunk.js"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/Todo-React/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "fd70799a4309e7db155a",
+    "url": "/Todo-React/static/css/main.68b2f618.chunk.css"
+  },
+  {
+    "revision": "08c0723de16cf8f74e7bb43b5f9cc8c9",
+    "url": "/Todo-React/index.html"
+  }
+];
